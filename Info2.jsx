31a412b7e0950154b7557d3f82e89755fb79880b
krishna6523 <86593071+krishna6523@users.noreply.github.com>
2021-06-28 13:14:33 +0530

    Add files via upload

---
+++ Info2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info2() {
+  return (
+    <div className="note">
+      <h1>Excel andData analytics</h1>
+      <p>Data analytics and dashboard</p>
+    </div>
+  );
+}
+export default Info2;
